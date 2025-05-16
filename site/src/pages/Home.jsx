eaa9dc5614ecc861a5438088096c305f4f8faed0
Eduardo Cesar <unkeniy8432@gmail.com>
2025-05-16 19:14:18 -0300

    Ajustes e preparações inciais do projeto

---
+++ site/src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import "./Home.scss";
+
+function Home() {
+  return <main></main>;
+}
+
+export default Home;
---
+++ site/src/routes.js
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home.jsx/Home.js";
+
+export default function Index() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
